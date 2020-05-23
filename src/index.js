c811107262cfb5e755b23f598942aed04f95de46
Elijah Tamarchenko <elijahtamarchenko@Elijahs-MacBook-Air.local>
2020-05-22 20:11:46 -0400

    Added Redux action

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './components/App';
 import store from './store';
 import { getReadableStories } from './selectors/story';
-import { STORY_ARCHIVE } from './constants/actionTypes';
+import { doArchiveStory } from './actions/archive';
 import * as serviceWorker from './serviceWorker';
 
 const stories= [
@@ -21,7 +21,7 @@ function render() {
   ReactDOM.render(
     <App
       stories={getReadableStories(store.getState())}
-      onArchive={id => store.dispatch({ type: STORY_ARCHIVE, id })}
+      onArchive={id => store.dispatch(doArchiveStory(id))}
     />,
     document.getElementById('root')
   );
