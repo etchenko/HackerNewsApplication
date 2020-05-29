90f792dd2b014eea37459ef39db946d863c54b98
Elijah Tamarchenko <elijahtamarchenko@Elijahs-MacBook-Air.local>
2020-05-28 21:12:53 -0400

    Updates

---
+++ precache-manifest.e9f9ae7410f7ea143e93ae0c5c5953c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32d1103a4b0eb1e72bfc635b84a4ecae",
+    "url": "/HackerNewsApplication/index.html"
+  },
+  {
+    "revision": "4ba8712e3a5d12f2069e",
+    "url": "/HackerNewsApplication/static/css/main.01b782a4.chunk.css"
+  },
+  {
+    "revision": "00bac3efd5138b1a324f",
+    "url": "/HackerNewsApplication/static/js/2.79667b98.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/HackerNewsApplication/static/js/2.79667b98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ba8712e3a5d12f2069e",
+    "url": "/HackerNewsApplication/static/js/main.ce24da37.chunk.js"
+  },
+  {
+    "revision": "c613e9beb7420325fb8e",
+    "url": "/HackerNewsApplication/static/js/runtime-main.ede69d58.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/HackerNewsApplication/precache-manifest.e9f9ae7410f7ea143e93ae0c5c5953c1.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/HackerNewsApplication/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
