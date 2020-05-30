c45fb4327d9f2e8e05890bf440e28f0b1b0a9bb2
Elijah Tamarchenko <elijahtamarchenko@Elijahs-MacBook-Air.local>
2020-05-30 19:42:15 -0400

    Fixed hackernews collection issue

--- src/api/story.js
+++ src/api/story.js
@@ -1,11 +1,11 @@
-const HN_BASE_URL = 'http://hn.algolia.com/api/v1/search?query=';
+const HN_BASE_URL = 'https://hn.algolia.com/api/v1/search?query=';
 
 const fetchStories = query =>
   fetch(HN_BASE_URL + query)
     .then(response => response.json());
 
 const getInitialStories = self =>
-  fetch("http://hn.algolia.com/api/v1")
+  fetch("https://hn.algolia.com/api/v1")
     .then(response => response.json());
 
 export {
