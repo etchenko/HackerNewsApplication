5f4da5ba8fdba1babfef939b05adeb0c87470e15
Elijah Tamarchenko <elijahtamarchenko@Elijahs-MacBook-Air.local>
2020-05-31 18:01:32 -0400

    Fixing Deployment issues

--- src/components/SearchStories.js
+++ src/components/SearchStories.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, Component  from 'react';
 import { connect } from 'react-redux';
 import { doFetchStories } from '../actions/story';
 import Button from './Button';
