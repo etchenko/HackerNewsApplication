8293593fb214808cb2ac20ad5757018736415a72
Elijah Tamarchenko <elijahtamarchenko@Elijahs-MacBook-Air.local>
2020-06-02 16:46:42 -0400

    Added Typescript

--- src/components/SearchStories.js
+++ src/components/SearchStories.js
@@ -1,5 +1,4 @@
 import React from 'react';
-//import Component   from 'react';
 import { connect } from 'react-redux';
 import { doFetchStories } from '../actions/story';
 import Button from './Button';
