873558d55760662f4a6051a40c559741fd7f7468
Elijah Tamarchenko <elijahtamarchenko@Elijahs-MacBook-Air.local>
2020-06-02 17:03:14 -0400

    Converted constants and App to jsx

---
+++ src/components/App.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react'
+import './App.css'
+import Stories from './Stories.js'
+import SearchStories from './SearchStories.js'
+
+export const App: React.FC = () =>
+  <div className="app">
+    <div className='interactions'>
+      <SearchStories />
+    </div>
+    <Stories />
+  </div>
---
+++ src/constants/actionTypes.tsx
@@ -0,0 +1,11 @@
+export const STORY_ARCHIVE = 'STORY_ARCHIVE';
+export type STORY_ARCHIVE = typeof STORY_ARCHIVE;
+
+export const STORIES_FETCH = 'STORIES_FETCH';
+export type STORIES_FETCH = typeof STORIES_FETCH;
+
+export const STORIES_ADD = 'STORIES_ADD';
+export type STORIES_ADD = typeof STORIES_ADD;
+
+export const STORIES_FETCH_ERROR = 'STORIES_FETCH_ERROR';
+export type STORIES_FETCH_ERROR = typeof STORIES_FETCH_ERROR;
