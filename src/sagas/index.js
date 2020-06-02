6b12f3ebeec45848e0e778d72930fcbb6d4f0718
Elijah Tamarchenko <elijahtamarchenko@Elijahs-MacBook-Air.local>
2020-06-02 00:55:49 -0400

    Fixed story fetching error

--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,6 +1,6 @@
 import  { takeEvery, all }  from 'redux-saga/effects';
 import  { STORIES_FETCH }  from '../constants/actionTypes';
-import  handleFetchStories  from './story';
+import  { handleFetchStories } from './story';
 
 function* watchAll() {
   yield all([
