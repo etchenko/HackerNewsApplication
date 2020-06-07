67f44a972d31d7bfccd646c94edbf6669fc2c68e
Elijah Tamarchenko <elijahtamarchenko@Elijahs-MacBook-Air.local>
2020-06-07 19:20:29 -0400

    Converted reducers to typescript

---
+++ src/reducers/archive.ts
@@ -0,0 +1,18 @@
+import { STORY_ARCHIVE }  from '../constants/actionTypes';
+//import { STATE_TYPE } from './story'
+
+const INITIAL_STATE: any[] = [];
+
+const applyArchiveStory = (state = INITIAL_STATE, action: any) =>
+  [ ...state, action.id];
+
+function archiveReducer(state = INITIAL_STATE, action: any) {
+  switch(action.type) {
+    case STORY_ARCHIVE : {
+      return applyArchiveStory(state, action,);
+    }
+    default : return state;
+  }
+}
+
+export default archiveReducer;
--- src/reducers/story.ts
+++ src/reducers/story.ts
@@ -1,21 +1,25 @@
 import { STORIES_ADD, STORIES_FETCH_ERROR } from '../constants/actionTypes';
 
+interface STATE_TYPE {
+  stories: any[];
+  error: unknown;
+}
 const INITIAL_STATE = {
   stories: [],
   error: null,
 };
 
-const applyAddStories = (state, action) => ({
+const applyAddStories = (state: STATE_TYPE, action: any) => ({
   stories: action.stories,
   error: null,
 });
 
-const applyFetchErrorStories = (state, action) => ({
+const applyFetchErrorStories = (state: STATE_TYPE, action: any) => ({
   stories: [],
   error: action.error,
 });
 
-function storyReducer(state = INITIAL_STATE, action) {
+function storyReducer(state = INITIAL_STATE, action: any) {
   switch(action.type) {
     case STORIES_ADD : {
       return applyAddStories(state, action);
